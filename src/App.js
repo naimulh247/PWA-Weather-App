45183acb238ff2fcb53d2eba849974d10f3cf4c2
naimulh247 <naimulh247@gmail.com>
2020-07-21 07:25:04 -0400

    src/App.js:29: +                    <div className="city-temp"> src/App.js:30: +                        {Math.round(weather.main.temp)} src/App.js:31: +                        <sup> &deg; C</sup> src/App.js:33: +                    </div>

--- src/App.js
+++ src/App.js
@@ -26,7 +26,11 @@ const App = () =>{
                         <span> {weather.name} </span>
                         <sup> {weather.sys.country} </sup>
                     </h2>
+                    <div className="city-temp">
+                        {Math.round(weather.main.temp)}
+                        <sup> &deg; C</sup>
 
+                    </div>
                 </div>
             )}
         

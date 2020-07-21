8197a2f19963d8a22e0bb95070c2a3f6992fd81e
naimulh247 <naimulh247@gmail.com>
2020-07-21 14:21:20 -0400

    changed URL to const

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-URL = 'https://api.openweathermap.org/data/2.5/weather';
+const URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = '7bc8d2add534e64a1d61f78518e037b0';
 
 const fetchWeather = async(query) => {

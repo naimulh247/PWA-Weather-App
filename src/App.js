faf6314b5c2dc79821c16303474893675d1748bf
naimulh247 <naimulh247@gmail.com>
2020-07-21 13:01:02 -0400

    src/App.js:36: -                        <img className="city-icon" src={`http://openweathermap.org/img/wn/10d@2x.png`} alt={weather.weather[0].description} /> src/App.js:36: +                        <img className="city-icon" src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0]

--- src/App.js
+++ src/App.js
@@ -33,7 +33,7 @@ const App = () =>{
                     </div>
 
                     <div className="info">
-                        <img className="city-icon" src={`http://openweathermap.org/img/wn/10d@2x.png`} alt={weather.weather[0].description} />
+                        <img className="city-icon" src={`http://openweathermap.org/img/wn/${weather.weather[0].icon}@2x.png`} alt={weather.weather[0].description} />
                         <p> {weather.weather[0].description} </p>
                     </div>
 

92331ca445c8ba58aa5a355723ff9d8188efbf0e
naimulh247 <naimulh247@gmail.com>
2020-07-21 13:15:18 -0400

    public/serviceworker.js:3: +const self = this; public/serviceworker.js:7: +self.addEventListener('install', (event)=> { public/serviceworker.js:8: + public/serviceworker.js:9: +}); public/serviceworker.js:13: +self.addEventListener('fetch', (event)=> { public/serviceworker.js:14: + public/serviceworker.js:15: +}); public/serviceworker.js:18: + public/serviceworker.js:19: +self.addEventListener('activate', (event)=> { public/serviceworker.js:20: + public/serviceworker.js:21: +}); public/serviceworker.js:22:  No newline at end of file

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -1,10 +1,21 @@
 const CACHE_NAME = "version-1";
 const urlsToCache = ['index.html', 'offline.html'];
+const self = this;
 
 // Install Service Worker
 
+self.addEventListener('install', (event)=> {
+
+});
 
 // Listen for Request 
 
+self.addEventListener('fetch', (event)=> {
+    
+});
 
 // Activate the Service Worker
+
+self.addEventListener('activate', (event)=> {
+    
+});

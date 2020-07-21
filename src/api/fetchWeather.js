8241c4eeb6e00274201b186572cfdc8986b766a3
naimulh247 <naimulh247@gmail.com>
2020-07-20 21:02:23 -0400

    src/api/fetchWeather.js:4: -URL = ""; src/api/fetchWeather.js:4: +URL = 'https://api.openweathermap.org/data/2.5/weather'; src/api/fetchWeather.js:5: +const API_KEY = '7bc8d2add534e64a1d61f78518e037b0';

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -1,7 +1,8 @@
 import axios from 'axios';
 
 
-URL = "";
+URL = 'https://api.openweathermap.org/data/2.5/weather';
+const API_KEY = '7bc8d2add534e64a1d61f78518e037b0';
 
 const fetchWeather = async(query) => {
     const response = await axios.get(URL)

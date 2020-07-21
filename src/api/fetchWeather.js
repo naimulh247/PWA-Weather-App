7aeef795939292aeb491996d442f00747efd436e
naimulh247 <naimulh247@gmail.com>
2020-07-20 21:45:22 -0400

    src/api/fetchWeather.js:8: -    const response = await axios.get(URL) src/api/fetchWeather.js:8: -} src/api/fetchWeather.js:8:  No newline at end of file src/api/fetchWeather.js:9: +    const {data} = await axios.get(URL, { src/api/fetchWeather.js:10: +        params: { src/api/fetchWeather.js:11: +            q: query, src/api/fetchWeather.js:12: +            units: 'metric', src/api/fetchWeather.js:13: +            APPID: API_KEY, src/api/fetchWeather.js:14: +        } src/api/fetchWeather.js:15: +    }); src/api/fetchWeather.js:16: +    return data; src/api/fetchWeather.js:17: +} src/api/fetchWeather.js:18: + src/api/fetchWeather.js:19: +export default fetchWeather; src/api/fetchWeather.js:20:  No newline at end of file

--- src/api/fetchWeather.js
+++ src/api/fetchWeather.js
@@ -5,5 +5,14 @@ URL = 'https://api.openweathermap.org/data/2.5/weather';
 const API_KEY = '7bc8d2add534e64a1d61f78518e037b0';
 
 const fetchWeather = async(query) => {
-    const response = await axios.get(URL)
+    const {data} = await axios.get(URL, {
+        params: {
+            q: query, 
+            units: 'metric',
+            APPID: API_KEY,
+        }
+    });
+    return data;
 }
+
+export default fetchWeather;

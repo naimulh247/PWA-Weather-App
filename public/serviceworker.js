1d4668df2e329d186d7bcc545e0e3ae0e4db9d62
naimulh247 <naimulh247@gmail.com>
2020-07-21 13:17:11 -0400

    public/serviceworker.js:10: -            .then(()=>{ public/serviceworker.js:10: +            .then((cache)=>{

--- public/serviceworker.js
+++ public/serviceworker.js
@@ -7,7 +7,7 @@ const self = this;
 self.addEventListener('install', (event)=> {
     event.waitUntil(
         caches.open(CACHE_NAME)
-            .then(()=>{
+            .then((cache)=>{
                 console.log('Opened Cache')
                 return cache.addAll(urlsToCache);
             })
